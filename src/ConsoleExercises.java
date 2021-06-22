373bcb63a821bb80fc4431d85bf048b9495e09c0
kicknback <joshua.borreli@gmail.com>
2021-06-22 16:19:16 -0500

    Finishing consoleExercises file

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,7 +4,7 @@ public class ConsoleExercises {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double pi = 3.14159;
-
+        sc.useDelimiter("\n");
 
 //        System.out.printf("The value of pi is approximately %.2f.", pi);
 //
@@ -21,10 +21,30 @@ public class ConsoleExercises {
 //
 //        System.out.printf("You entered the following 3 words:\n%s\n%s\n%s", first, second, third);
 
-        System.out.print("Enter a sentence, por favor: ");
+//        System.out.print("Enter a sentence, por favor: ");
+//
+//        String sentence = sc.nextLine();
+//        System.out.printf("You entered the following sentence: \n\t%s", sentence);
 
-        String sentence = sc.nextLine();
-        System.out.printf("You entered the following sentence: \n\t%s", sentence);
+//        System.out.println("Enter room dimensions to calculate area and perimeter: ");
+//        int length = Integer.parseInt(sc.next());
+//        int width = Integer.parseInt(sc.next());
+//        int area = length * width;
+//        int perimeter = (length * 2) + (width * 2);
+//        System.out.printf("The area of your input is %s, while the perimeter is %s.", area, perimeter);
+
+
+        System.out.println("Enter room dimensions to calculate area and perimeter: ");
+
+        double length = Double.parseDouble(sc.next());
+        double width = Double.parseDouble(sc.next());
+        double heigth = Double.parseDouble((sc.next()));
+        double area = length * width;
+        double perimeter = (length * 2) + (width * 2);
+        double volume = length * width * heigth;
+
+        System.out.printf("The area of your input is %.3f, while the perimeter is %.3f.\nAlso, the volume is %.3f", area, perimeter, volume);
+
 
 
     }

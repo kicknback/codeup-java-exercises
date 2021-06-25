d5d00fab7304b34f790dd9c82d9a25037bd6c969
kicknback <joshua.borreli@gmail.com>
2021-06-24 22:38:33 -0500

    Updating control flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -51,6 +51,19 @@ public class ControlFlowExercises {
 //        }
 
 
+//        for (byte i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("Fizz Buzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+
 
     }
 }

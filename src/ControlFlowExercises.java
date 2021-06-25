658fae84d07c605f6881d73bcb7219f995433e7f
kicknback <joshua.borreli@gmail.com>
2021-06-24 22:15:07 -0500

    Updating control flow exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,12 +1,56 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-        int i = 5;
+//        int i = 5;
+//
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
 
-        while (i <= 15) {
-            System.out.println(i);
-            i++;
-        }
+//        short num = 0;
+//
+//        do {
+//            System.out.println(num);
+//            num += 2;
+//        } while (num <= 100);
+
+//        short num = 100;
+//
+//        do {
+//            System.out.println(num);
+//            num -= 5;
+//        } while (num >= -10);
+
+
+//        This is somehow giving me an infinite loop
+//        int i = 2;
+//
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        } while (i < 1000000);
+
+        //-- For loop version of previous 2 exercises.
+
+//        for (byte i = 5; i <= 15; i++) {
+//            System.out.println(i);
+//        }
+
+//        for (byte i = 0; i <= 100; i += 2) {
+//            System.out.println(i);
+//        }
+
+//        for (short num = 100; num >= -10; num -= 5) {
+//            System.out.println(num);
+//        }
+
+        //This is also giving me an infinite loop
+//        for (int i = 2; i < 1000000; i*=i) {
+//            System.out.println(i);
+//        }
+
+
 
     }
 }

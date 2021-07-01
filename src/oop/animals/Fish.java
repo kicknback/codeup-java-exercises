405dce540848d5f20461d91ec25b92a23f372833
kicknback <joshua.borreli@gmail.com>
2021-07-01 12:02:30 -0500

    Adding inheritance lecture classes

---
+++ src/oop/animals/Fish.java
@@ -0,0 +1,25 @@
+package oop.animals;
+
+public class Fish extends MarineAnimal {
+
+    private boolean hasScales;
+
+    public Fish(boolean canSwim, int length, int finCount, boolean isSaltWater, String name, boolean hasScales) {
+        super(canSwim, length, finCount, isSaltWater, name);
+        this.hasScales = hasScales;
+    }
+
+    public Fish(String name) {
+        super(name);
+    }
+
+    public Fish(){};
+
+    public boolean isHasScales() {
+        return hasScales;
+    }
+
+    public void setHasScales(boolean hasScales) {
+        this.hasScales = hasScales;
+    }
+}
---
+++ src/oop/animals/SomethingsFishy.java
@@ -0,0 +1,11 @@
+package oop.animals;
+
+public class SomethingsFishy {
+    public static void main(String[] args) {
+
+        MarineAnimal marineAnimal = new AquaMammal("Blue whale");
+
+        System.out.println(marineAnimal.getName());
+
+    }
+}

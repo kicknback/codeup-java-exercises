c7e907e539f4027e57f4e0c7f6950c710e1de1a6
kicknback <joshua.borreli@gmail.com>
2021-07-08 16:56:10 -0500

    Lecture mod

--- src/ArrayLecture.java
+++ src/ArrayLecture.java
@@ -5,19 +5,30 @@ public class ArrayLecture {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] intArray = getIntArray();
-        intArray = Arrays.stream(intArray).map(i -> i * 2).toArray();
+//        int[] intArray = getIntArray();
+//        intArray = Arrays.stream(intArray).map(i -> i * 2).toArray();
 
 //    System.out.println(Arrays.toString(intArray));
-        System.out.println("How many names do you have?: ");
-        int nameCount = sc.hasNextInt() ? sc.nextInt() : 1;
-        System.out.println(getFullName(nameCount, sc));
+//        System.out.println("How many names do you have?: ");
+//        int nameCount = sc.hasNextInt() ? sc.nextInt() : 1;
+//        System.out.println(getFullName(nameCount, sc));
 
+//        String[] someNames = getFullName(nameCount, sc);
+//        String[] moreNames = Arrays.copyOf(someNames, someNames.length);
+
+//        System.out.println(Arrays.toString(moreNames));
+//        System.out.println(Arrays.equals(someNames, moreNames));
+
+
+        int[] intArr = getIntArray();
+        Arrays.sort(intArr);
+
+        System.out.println(Arrays.toString(intArr));
     }
 
 
     public static int[] getIntArray() {
-        return new int[]{1, 2, 3};
+        return new int[]{3, 2, 1};
     }
 
     public static int[] getIntArray(int length) {
@@ -28,13 +39,14 @@ public class ArrayLecture {
         return intArray;
     }
 
-    public static String getFullName(int names, Scanner sc) {
+    public static String[] getFullName(int names, Scanner sc) {
         String[] fullName = new String[names];
         for (int i = 0; i < names; i++) {
             System.out.println("\nEnter name: ");
             fullName[i] = sc.next();
         }
-        return "\n" + Arrays.toString(fullName);
+//        return "\n" + Arrays.toString(fullName);
+        return  fullName;
     }
 
 }

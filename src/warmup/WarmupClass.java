28c53ef742de707931345c62f1aba3e63a593262
kicknback <joshua.borreli@gmail.com>
2021-07-16 23:26:06 -0500

    Started warmup gettwenty primes.. will finish

--- src/warmup/WarmupClass.java
+++ src/warmup/WarmupClass.java
@@ -86,4 +86,23 @@ public class WarmupClass {
 
     }
 
+//    Write a method named getTwentyPrimes() that returns a string containing the first 20 prime
+//    numbers, each separated by a comma. Output: "1, 2, 3, 5, 7, 11, 13, 17, 19"...
+//    until we have a total count of 20 primes in the string.
+
+    public static boolean isPrime
+
+    public static String getTwentyPrimes() {
+        int num = 100;
+        List<Integer> primeList = new ArrayList<Integer>();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j % 1 == 0) {
+
+                }
+            }
+        }
+    }
+
 }
